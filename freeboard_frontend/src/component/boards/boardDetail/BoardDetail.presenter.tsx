3b2240397047e169d0b41010a6eb1d95849ebdc8
JeongTaekCho <92679073+JeongTaekCho@users.noreply.github.com>
2022-09-13 09:45:40 +0900

    programmers day 4

--- freeboard_frontend/src/component/boards/boardDetail/BoardDetail.presenter.tsx
+++ freeboard_frontend/src/component/boards/boardDetail/BoardDetail.presenter.tsx
@@ -234,9 +234,9 @@ const BoardDetailUi = ({
                 </S.DefaultBtn>
                 <S.DefaultBtn id={comment?._id} onClick={onCommentPsModal}>
                   <svg
-                    width="14"
-                    height="14"
-                    viewBox="0 0 14 14"
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                   >
