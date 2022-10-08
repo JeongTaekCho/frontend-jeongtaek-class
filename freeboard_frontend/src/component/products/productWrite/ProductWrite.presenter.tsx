d27cdbca5cc73c44488fbc1aef801dab8f6656b9
JeongTaekCho <92679073+JeongTaekCho@users.noreply.github.com>
2022-10-08 22:58:04 +0900

    dev productWrite

--- freeboard_frontend/src/component/products/productWrite/ProductWrite.presenter.tsx
+++ freeboard_frontend/src/component/products/productWrite/ProductWrite.presenter.tsx
@@ -1,6 +1,11 @@
+import { useEffect, useState } from "react";
 import InputDefault from "../../common/inputs/inputDefault";
 import * as S from "./ProductWrite.styled";
 import { IProductWriteUi } from "./ProductWrite.types";
+
+declare const window: typeof globalThis & {
+  kakao: any;
+};
 
 const ProductWriteUi = ({
   onSubmitProduct,
@@ -8,6 +13,58 @@ const ProductWriteUi = ({
   handleSubmit,
   onChangeFile,
 }: IProductWriteUi) => {
+  const [lat, setLat] = useState("");
+  const [lng, setLng] = useState("");
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=67ff797434525aa2bbca4bf944af63c8";
+
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById("map"); // 지도를 담을 영역의 DOM 레퍼런스
+        const options = {
+          // 지도를 생성할 때 필요한 기본 옵션
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표.
+          level: 3, // 지도의 레벨(확대, 축소 정도)
+        };
+
+        const map = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+
+        var marker = new window.kakao.maps.Marker({
+          // 지도 중심좌표에 마커를 생성합니다
+          position: map.getCenter(),
+        });
+        // 지도에 마커를 표시합니다
+        marker.setMap(map);
+
+        // 지도에 클릭 이벤트를 등록합니다
+        // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+        window.kakao.maps.event.addListener(
+          map,
+          "click",
+          function (mouseEvent) {
+            // 클릭한 위도, 경도 정보를 가져옵니다
+            const latlng = mouseEvent.latLng;
+
+            // 마커 위치를 클릭한 위치로 옮깁니다
+            marker.setPosition(latlng);
+
+            // var message = "클릭한 위치의 위도는 " + latlng.getLat() + " 이고, ";
+            // message += "경도는 " + latlng.getLng() + " 입니다";
+
+            // var resultDiv = document.getElementById("clickLatlng");
+            // resultDiv.innerHTML = message;
+            setLat(latlng.getLat());
+            setLng(latlng.getLng());
+          }
+        );
+      });
+    };
+  }, []);
   return (
     <>
       <S.ProductWrapper>
@@ -66,7 +123,12 @@ const ProductWriteUi = ({
               <S.AddressInfoBox>
                 <S.Ptitle>GPS</S.Ptitle>
                 <S.GpsInfoBox>
-                  <S.GpsInput type="text" placeholder="위도(LAT)" />
+                  <S.GpsInput
+                    type="text"
+                    placeholder="위도(LAT)"
+                    value={Number(lat)}
+                    {...register("useditemAddress.lat")}
+                  />
                   <svg
                     width="14"
                     height="20"
@@ -79,7 +141,12 @@ const ProductWriteUi = ({
                       fill="#FFD600"
                     />
                   </svg>
-                  <S.GpsInput type="text" placeholder="경도(LNG)" />
+                  <S.GpsInput
+                    type="text"
+                    placeholder="경도(LNG)"
+                    value={Number(lng)}
+                    {...register("useditemAddress.lng")}
+                  />
                 </S.GpsInfoBox>
                 <S.ProductAddressContainer>
                   <S.Ptitle>주소</S.Ptitle>
@@ -105,7 +172,7 @@ const ProductWriteUi = ({
                     <S.ProductFile
                       id={`file${index + 1}`}
                       type="file"
-                      title={index}
+                      title={String(index)}
                       onChange={onChangeFile(index)}
                     />
                   </div>
