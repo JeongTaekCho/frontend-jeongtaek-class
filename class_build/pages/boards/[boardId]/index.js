3e7f8285dffeba29eb1a895f9d9ced55c271b5c1
JeongTaekCho <92679073+JeongTaekCho@users.noreply.github.com>
2022-10-25 12:51:53 +0900

    update build

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -1,3 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function BoardPage() {
-  return <div>안녕하세요 게시판 입니다. 동적페이지에요</div>;
+  const router = useRouter();
+
+  return (
+    <div>
+      안녕하세요 게시판 입니다. 동적페이지에요!
+      <br /> 게시글 아이디 : {router.query.boardId}
+    </div>
+  );
 }
